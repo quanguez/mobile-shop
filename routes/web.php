648537e8320848e72e2dfa8a8092b91d8cccb1f9
Lucas <hexarceus@gmail.com>
2024-03-21 17:32:13 +0700

    Add design for pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\HomePage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\ProductDetailPage;
+use App\Livewire\CartPage;
+use App\Livewire\CheckoutPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\ForgotPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\CancelPage;
+use App\Livewire\SuccessPage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\OrderDetailPage;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +28,17 @@ use App\Livewire\HomePage;
 */
 
 Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductsPage::class);
+Route::get('/products/{product}', ProductDetailPage::class);
+Route::get('/cart', CartPage::class);
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/login', LoginPage::class);
+Route::get('/register', RegisterPage::class);
+Route::get('/forgot', ForgotPage::class);
+Route::get('/reset-password', ResetPasswordPage::class);
+Route::get('/my-orders', MyOrdersPage::class);
+Route::get('/my-orders/{order}', OrderDetailPage::class);
