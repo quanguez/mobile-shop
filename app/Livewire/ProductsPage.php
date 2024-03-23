d0c3a6465f28c53ed3922f3715107f2e67ca42b2
Lucas <hexarceus@gmail.com>
2024-03-23 14:00:28 +0700

    Dynamic data ProductPage and ProductDetailPage

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Product - DewShop')]
 class ProductsPage extends Component
 {
     public function render()
     {
-        return view('livewire.products-page');
+        $categories = Category::where('is_active', 1)->get(['id', 'name', 'slug']);
+        $brands = Brand::where('is_active', 1)->get(['id', 'name', 'slug']);
+        $productsQuery = Product::query()->where('is_active', 1);
+        return view('livewire.products-page', [
+            'categories' => $categories,
+            'brands' => $brands,
+            'products' => $productsQuery->paginate(6),
+        ]);
     }
 }
